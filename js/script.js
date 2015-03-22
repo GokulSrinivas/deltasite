0c1f4083b7a7b0f6828dc0bc7d693ce20a4ddd60
Gokul Srinivas <gokusrinivas@gmail.com>
2015-03-22 17:29:25 +0530

    Added Carousel for projects. Need to add stuff for award. Need to add content for about delta. Add footer

--- js/script.js
+++ js/script.js
@@ -8,10 +8,49 @@ $(document).ready(function(){
        	typeSpeed: 0
     });
 	
+	var carouselshift =0 ;
+	var interval;
+	var inc = 0;
+	var carouselwidth = ($('#carousel > article').length)*320 + 20 ;
+	$('#carousel').css('width',(carouselwidth)+'px');
+	setInterval(function(){
+
+		if(inc<0) inc--;
+		if(inc>0) inc++;
+		var scwidth = window.innerWidth;
+		
+		if(((inc>0)&&(carouselshift<0))||((inc<0)&&(carouselshift>-carouselwidth+scwidth)))
+			carouselshift += inc;
+		
+		else
+		{
+			if(carouselshift>0) carouselshift =0 ;
+			if(carouselshift<-carouselwidth+scwidth) carouselshift = -carouselwidth+scwidth;
+		}
+
+
+		$('#carousel').css('transform','translate('+carouselshift+'px,0px)');	
+	},1000/60);
+
 	$("#goDown").click(function(){
 		$("#aboutlink").trigger('click');
 	});
 
+	$('#back').hover(function(){
+		// (carouselshift==0) return;
+		inc=10;
+	});
+	$('#front').hover(function(){
+		// (carouselshift==carouselwidth) return;
+		inc=-10;
+	});
+
+	$('#back').mouseout(function(){
+		inc=0;	
+	});
+	$('#front').mouseout(function(){
+		inc=0;	
+	});
 //  Binding the motions for scrolling 
 	$(window).scroll(0); // Scroll to landing page when page loads.
 	$('#scrolltotop').hide();
